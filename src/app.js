ba6d465c1606a070a074e232b4ce86389ec60414
Rajanish Singh <rajanishsingh729@email.com>
2026-06-12 21:55:46 +0530

    Railway database connected

--- src/app.js
+++ src/app.js
@@ -1,9 +1,31 @@
 const express = require("express");
 const githubRouter = require("./routes/githubRoutes");
+const pool = require("./config/db");
 
 
 const app = express();
 app.use(express.json());
+
+
+
+app.get("/db-test", async (req, res) => {
+  try {
+    const [rows] = await pool.query("SELECT NOW() as currentTime");
+
+    res.json({
+      success: true,
+      data: rows
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      error: error.message
+    });
+  }
+});
+
+
 
 app.use("/api/github",githubRouter);
 
