7a195a37afec33d35b77ebfa44b56e884ef474f7
Rajanish Singh <rajanishsingh729@email.com>
2026-06-13 19:05:22 +0530

    Added GitHub token authentication

--- src/services/githubService.js
+++ src/services/githubService.js
@@ -1,5 +1,5 @@
 const axios = require("axios");
-
+require("dotenv").config();
 
 
 const getGithubProfile = async (userName)=>{
@@ -7,9 +7,25 @@ const getGithubProfile = async (userName)=>{
 
     //    console.log("called");
         
-        const response = await axios.get(`https://api.github.com/users/${userName}`);
-        // console.log(response.data);
+        // const response = await axios.get(`https://api.github.com/users/${userName}`);
+        // // console.log(response.data);
+        // return response.data;
+
+        // after rate limit over of github api
+
+        const response = await axios.get(
+        `https://api.github.com/users/${userName}`,
+        {
+            headers: {
+            Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+            Accept: "application/vnd.github+json",
+            "User-Agent": "github-profile-analyzer"
+            }
+        }
+        );
+
         return response.data;
+
 
     } catch (error) {
         console.log("Error" + error);
